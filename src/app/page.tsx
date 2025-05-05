8b2468f72eaa82e9cbec3ed9ff0df718e4d6da4a
BijanProgrammer <bijaneisapour@gmail.com>
2025-05-05 16:01:29 +0330

    [INIT]

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <h1>سلام، رفیق!</h1>
+      <p>123</p>
+    </div>
+  );
+}
